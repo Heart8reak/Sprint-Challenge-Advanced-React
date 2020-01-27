6bfe4149246134275f546ca7b0d28e1befebd499
Carlos Baez <carlosbaez@Green-Planet.attlocal.net>
2020-01-26 22:55:23 -0500

    updated the title

--- client/src/App.js
+++ client/src/App.js
@@ -7,8 +7,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Sprint Challenge-Advance React</h1>
-        <h3>Woman's World Cup with the most Google Searches</h3>
+        {/* <h1>Sprint Challenge-Advance React</h1> */}
+        <h2>Woman's World Cup with the most Google Searches</h2>
         <br />
         <Home />
       </header>
