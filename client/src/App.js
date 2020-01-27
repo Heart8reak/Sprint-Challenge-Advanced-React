1185c167de62181b76e77532daa6c6d87d6752ac
Carlos Baez <carlosbaez@Green-Planet.attlocal.net>
2020-01-26 22:51:27 -0500

    changed the title

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Sprint Challenge-Advance React</h1>
-        <h3>Players with the most Google Searches</h3>
+        <h3>Woman's World Cup with the most Google Searches</h3>
         <br />
         <Home />
       </header>
